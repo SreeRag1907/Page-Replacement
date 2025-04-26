37be367f242c281a21612cfd75d1550670b53f9e
Sree-1907 <c.sreerag17@gmail.com>
2025-04-26 20:26:36 +0530

    made the refernce string short

--- components/page-replacement-visualizer.tsx
+++ components/page-replacement-visualizer.tsx
@@ -30,7 +30,7 @@ interface StepState {
 }
 
 const PageReplacementVisualizer = () => {
-  const [referenceString, setReferenceString] = useState("7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1")
+  const [referenceString, setReferenceString] = useState("7 0 1 2 0 3 0 4 2 3 0 3 2 1 2")
   const [parsedReferenceString, setParsedReferenceString] = useState<number[]>([])
   const [frameCount, setFrameCount] = useState(3)
   const [algorithm, setAlgorithm] = useState<AlgorithmType>("fifo")
